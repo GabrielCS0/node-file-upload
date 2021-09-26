21de0435712e743c84bb8654fba628e28baddb3c
Gabriel <gabrielcemp@gmail.com>
2021-09-26 09:54:05 -0300

    feat: add custom error class

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,30 @@
-import express from 'express'
+import express, { Request, Response, NextFunction } from 'express'
+import morgan from 'morgan'
+import { router } from './routes'
+import { AppError } from './errors/AppError'
 
 const app = express()
 
 app.use(express.json())
+app.use(router)
+app.use(morgan('dev'))
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err: Error, req: Request, res: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({
+      status: 'error',
+      message: err.message
+    })
+  }
+
+  console.error(err)
+
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal Server Error'
+  })
+})
 
 app.listen(process.env.PORT || 5000, () =>
   console.log(`Server is running on port ${process.env.PORT || 5000}`)
